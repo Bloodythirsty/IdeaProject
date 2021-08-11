99effdad0d39b64698d587bce9b3e484635ee674
zhangkangkang <zhangkangkang@sogou-inc.com>
2021-08-11 10:11:58 +0800

    0211-sougou

--- algorithm/leetCode/src/_114二叉树展开链表.java
+++ algorithm/leetCode/src/_114二叉树展开链表.java
@@ -1,27 +1,27 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-
-public class _114二叉树展开链表 {
-    public static void main(String[] args) {
-
-    }
-
-    public void flatten(TreeNode root) {
-        if(root == null) return ;
-        Deque<TreeNode> stack = new ArrayDeque<>();
-        TreeNode p = new TreeNode(0);
-        while(root != null || !stack.isEmpty()){
-            while(root != null){
-                p.right = root;
-                p.left = null;
-                p = p.right;
-                if(root.right != null){
-                    stack.push(root.right);
-                }
-                root = root.left;
-            }
-            root = stack.pop();
-            // root = root.right;
-        }
-    }
-}
+//import java.util.ArrayDeque;
+//import java.util.Deque;
+//
+//public class _114二叉树展开链表 {
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    public void flatten(TreeNode root) {
+//        if(root == null) return ;
+//        Deque<TreeNode> stack = new ArrayDeque<>();
+//        TreeNode p = new TreeNode(0);
+//        while(root != null || !stack.isEmpty()){
+//            while(root != null){
+//                p.right = root;
+//                p.left = null;
+//                p = p.right;
+//                if(root.right != null){
+//                    stack.push(root.right);
+//                }
+//                root = root.left;
+//            }
+//            root = stack.pop();
+//            // root = root.right;
+//        }
+//    }
+//}
